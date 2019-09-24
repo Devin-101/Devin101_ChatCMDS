bca3b3e23acf4aaa1562e5401263093856562298
Devin-101 <55755950+Devin-101@users.noreply.github.com>
2019-09-24 10:50:08 -0500

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,13 @@
+Citizen.CreateThread(function()
+    TriggerEvent('chat:addSuggestion', '/ooc', 'Usage: /ooc [message], sends out an out of character message with your player name to nearby players')
+    TriggerEvent('chat:addSuggestion', '/me', 'Proximity-based chat that is to be used during roleplay. Usage: /me [message]')
+    TriggerEvent('chat:addSuggestion', '/911', 'Usage: /911 [message], sends out a 911 Call too all Officers and Fire/EMS')
+    TriggerEvent('chat:addSuggestion', '/news', 'Sends a News Message. Usage: /news [message]')
+    TriggerEvent('chat:addSuggestion', '/ad', 'Usage: /add [message], sends out an AD To the whole server')
+    TriggerEvent('chat:addSuggestion', '/twt', 'Sends a Message on Twotter. Usage: /twt [message]')
+    TriggerEvent('chat:addSuggestion', '/mdt', 'Runs Person Name in the CAD/MDT')
+    TriggerEvent('chat:addSuggestion', '/src', 'Searches Players Car')
+    TriggerEvent('chat:addSuggestion', '/srp', 'Searches Person For Anything on them')
+
+ end)
+
